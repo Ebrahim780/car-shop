12c1f81869a6351716961fe9ecf1d6e6ee63fd71
Ebrahim <akbrly42@gmail.com>
2022-05-11 11:27:25 +0430

    create login & signup form

--- js/form.js
+++ js/form.js
@@ -1,7 +1,14 @@
-function checkValue(put) {
-    var value = put.value
-    if(!value == ''){
-        put.border = '1px solid orangered'
-        put.color = 'orangered'
+const login = document.querySelector('.login')
+const signup = document.querySelector('.signup')
+
+const disableBox = (show, hide) => {
+    let boxes = document.querySelectorAll('.form__box')
+    for (let i = 2; i < boxes.length - 1; i++) {
+        boxes[i].classList.toggle('hidden')
     }
+    show.classList.toggle('active')
+    hide.classList.toggle('active')
 }
+
+login.addEventListener('click',() => disableBox(login, signup))
+signup.addEventListener('click', () => disableBox(signup, login))
