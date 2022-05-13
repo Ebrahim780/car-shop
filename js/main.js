b8042c2eb47a1f5e00498017a26d5a49fce63a63
Ebrahim <akbrly42@gmail.com>
2022-05-13 16:47:31 +0430

    change close mode for menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 const menu = document.querySelector('.header__menu__image')
 const mobile = document.querySelector('.header__navigation')
 const check = document.getElementById('check')
-const exit = document.getElementById('close')
+const fade = document.querySelector('.header__fade')
 
-menu.onclick = function () {
+menu.onclick = () => {
     mobile.setAttribute('class', 'list__mobile')
+    fade.classList.add('back__drop')
 }
 
-exit.onclick = function () {
+fade.onclick = () => {
     mobile.setAttribute('class', 'header__navigation')
+    fade.classList.remove('back__drop')
 }
 
-check.onclick = function () {
+check.onclick = () => {
     document.querySelector('.container').classList.toggle('dark')
     document.querySelector('.sidebar').classList.toggle('dark')
 }
