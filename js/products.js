30671def39a74f2395fd9de2c8ce6676ad71e9ae
Ebrahim <akbrly42@gmail.com>
2022-05-16 19:21:13 +0430

    add filter feature (with some minor bugs )

--- js/products.js
+++ js/products.js
@@ -1,34 +1,92 @@
-fetch('js/data.json')
-  .then(Response => Response.json())
-  .then(products => products.map(product => {
-    document.querySelector('.products').innerHTML += `
-    <div class='product'>
-      <img src='${product.url}' class='product__image' />
-      <div class='product__content'>
-        <span>نام: </span>
-        <span>${product.name}</span>
-      </div>
-      <div class='product__content'>
-        <span>نوع: </span>
-        <span>${product.category}</span>
-      </div>
-      <div class='product__content'>
-        <span>وضعیت: </span>
-        <span>${product.status}</span>
-      </div>
-      <div class='product__content'>
-        <span>سال ساخت: </span>
-        <span>${product.year}</span>
-      </div>
-      <div class='product__content'>
-        <span>قیمت: </span>
-        <span>${product.price}</span>
-      </div>
-      <div class='product__content'>
-        <span>موجود: </span>
-        <span>${product.available ? '✔' : '❌'}</span>
-      </div>
-      <button class='button'>سفارش</button>
-    </div>
-    `
-  }))
+const div = document.createElement('div')
+div.classList.add('products')
+document.querySelector('.content').append(div)
+
+const select = document.querySelector('select')
+const radios = document.querySelectorAll('.filter__radio')
+const inputs = document.querySelectorAll('.filter__input')
+const checks = document.querySelectorAll('.filter__check')
+
+const filterProducts = (value, key) => {
+  let filteredValue = null
+
+  fetch('js/data.json')
+    .then(Response => Response.json())
+    .then(products => {
+      switch (key) {
+        case 'category':
+          filteredValue = products.filter(product => product.category === value)
+          console.log(filteredValue)
+          break
+
+        case 'status':
+          filteredValue = products.filter(product => product.status === value)
+          break
+
+        case 'fromYear':
+          filteredValue = products.filter(product => product.year >= value)
+          break
+
+        case 'toYear':
+          filteredValue = products.filter(product => product.year <= value)
+          break
+
+        case 'noneAdopted':
+          filteredValue = products.filter(product => product.price != value)
+          console.log(value)
+          break
+
+        case 'available':
+          filteredValue = products.filter(product => product.available == true)
+          break
+
+        default:
+          filteredValue = products
+      }
+
+      div.innerHTML = ''
+      filteredValue.map(product => {
+        div.innerHTML += `
+        <div class='product'>
+          <img src='${product.url}' class='product__image' />
+          <div class='product__content'>
+            <span>نام: </span>
+            <span>${product.name}</span>
+          </div>
+          <div class='product__content'>
+            <span>نوع: </span>
+            <span>${product.category}</span>
+          </div>
+          <div class='product__content'>
+            <span>وضعیت: </span>
+            <span>${product.status}</span>
+          </div>
+          <div class='product__content'>
+            <span>سال ساخت: </span>
+            <span>${product.year}</span>
+          </div>
+          <div class='product__content'>
+            <span>قیمت: </span>
+            <span>${product.price}</span>
+          </div>
+          <div class='product__content'>
+            <span>موجود: </span>
+            <span>${product.available ? '✔' : '❌'}</span>
+          </div>
+          <button class='button'>سفارش</button>
+        </div>
+        `
+      })
+    })
+}
+
+filterProducts()
+select.onchange = event => filterProducts(event.target.value, 'category')
+radios[0].onchange = event => filterProducts(event.target.value, 'status')
+radios[1].onchange = event => filterProducts(event.target.value, 'status')
+// TODO fix box
+inputs[0].onchange = event => filterProducts(event.target.value, 'fromYear')
+inputs[1].onchange = event => filterProducts(event.target.value, 'toYear')
+// TODO fix box
+checks[0].onchange = event => filterProducts(event.target.value, 'noneAdopted')
+checks[1].onchange = () => filterProducts(null, 'available')
