de897a06bf178a898592d903fa631b3f5bc9e0a2
Ebrahim <akbrly42@gmail.com>
2022-05-30 09:39:21 +0430

    add cart to project

---
+++ js/cart.js
@@ -0,0 +1,62 @@
+const div = document.createElement('div')
+div.classList.add('cart')
+document.querySelector('.content').append(div)
+
+let orderList = JSON.parse(localStorage.getItem('items'))
+
+const showCart = () => {
+  fetch('js/data.json')
+    .then(res => res.json())
+    .then(products => {
+      div.innerHTML = ''
+      if (orderList.length)
+        products.map(product => {
+          if (orderList.includes(Number(product.id)))
+            div.innerHTML += `
+      <div class='card'>
+        <img src='${product.url}' class='card__image' />
+        <div class='card__content'>
+          <span>نام: </span>
+          <span>${product.name}</span>
+        </div>
+        <div class='card__content'>
+          <span>نوع: </span>
+          <span>${product.category}</span>
+        </div>
+        <div class='card__content'>
+          <span>وضعیت: </span>
+          <span>${product.status}</span>
+        </div>
+        <div class='card__content'>
+          <span>سال ساخت: </span>
+          <span>${product.year}</span>
+        </div>
+        <div class='card__content'>
+          <span>قیمت: </span>
+          <span>${product.price}</span>
+        </div>
+        <div class='card__content'>
+          <span>موجود: </span>
+          <span>${product.available ? '✅' : '❌'}</span>
+        </div>
+        <div class='card__box__button'>
+          <button class='button button__danger' onclick='removeItem(${product.id})'>حذف</button>
+          <button class='button button__success'>پرداخت</button>
+        </div>
+      </div>
+      `})
+
+      else div.innerHTML += `<div class='cart__empty'>
+      <img class='empty__image' src='icon/public/empty.svg' />
+      <h2 class='empty__content'>سبد خرید شما خالی است</h2>
+      </div>`
+    })
+}
+
+showCart()
+
+const removeItem = id => {
+  orderList = orderList.filter(item => item !== id)
+  localStorage.setItem('items', JSON.stringify(orderList))
+  showCart()
+}
--- js/products.js
+++ js/products.js
@@ -6,6 +6,13 @@ const select = document.querySelector('select')
 const radios = document.querySelectorAll('.filter__radio')
 const inputs = document.querySelectorAll('.filter__input')
 const checks = document.querySelectorAll('.filter__check')
+let orderList = JSON.parse(localStorage.getItem('items'))
+
+const order = id => {
+  if (!orderList.includes(id))
+    orderList.push(id)
+  localStorage.setItem('items', JSON.stringify(orderList))
+}
 
 const filterProducts = (value, key) => {
   let filteredValue = null
@@ -45,33 +52,33 @@ const filterProducts = (value, key) => {
       div.innerHTML = ''
       filteredValue.map(product => {
         div.innerHTML += `
-        <div class='product'>
-          <img src='${product.url}' class='product__image' />
-          <div class='product__content'>
+        <div class='card'>
+          <img src='${product.url}' class='card__image' />
+          <div class='card__content'>
             <span>نام: </span>
             <span>${product.name}</span>
           </div>
-          <div class='product__content'>
+          <div class='card__content'>
             <span>نوع: </span>
             <span>${product.category}</span>
           </div>
-          <div class='product__content'>
+          <div class='card__content'>
             <span>وضعیت: </span>
             <span>${product.status}</span>
           </div>
-          <div class='product__content'>
+          <div class='card__content'>
             <span>سال ساخت: </span>
             <span>${product.year}</span>
           </div>
-          <div class='product__content'>
+          <div class='card__content'>
             <span>قیمت: </span>
             <span>${product.price}</span>
           </div>
-          <div class='product__content'>
+          <div class='card__content'>
             <span>موجود: </span>
             <span>${product.available ? '✅' : '❌'}</span>
           </div>
-          <button class='button'>سفارش</button>
+          <button class='button' onclick="order(${product.id})">سفارش</button>
         </div>
         `
       })
