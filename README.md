5c6bb12f053adc5e4030cb0694369531bb424cfc
Ebrahim <akbrly42@gmail.com>
2023-09-30 11:22:55 +0330

    add demo image and description to readme file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Mamooth Shop
-## Mamooth Shop project repository Paas
+# Mammoth Shop
+## A template of simple e-commerce website created with Pure-CSS and Vanilla-Js
+
+## Showcase
+![Mammoth Shop Showcase](https://iili.io/HSbCT3Q.png)
--- js/main.js
+++ js/main.js
@@ -3,36 +3,39 @@ const mobile = document.querySelector('.header__navigation')
 const check = document.getElementById('check')
 const fade = document.querySelector('.header__fade')
 
-let mode = false
+let mode = 'light'
 let modeStatus = JSON.parse(localStorage.getItem('mode'))
 
 const changeMode = () => {
-	let lights = document.querySelectorAll('.light')
-	let lightens = document.querySelectorAll('.lighten')
+  let lights = document.querySelectorAll('.light')
+  let lightens = document.querySelectorAll('.lighten')
 
-	for (let light of lights) light.classList.toggle('dark')
+  for (let light of lights) light.classList.toggle('dark')
 
-	for (let lighten of lightens) lighten.classList.toggle('darken')
+  for (let lighten of lightens) lighten.classList.toggle('darken')
 
-	mode = !mode
-	localStorage.setItem('mode', JSON.stringify(mode))
+  mode === 'light' ? (mode = 'dark') : (mode = 'light')
+
+  document.querySelector(':root').style.colorScheme = mode
+
+  localStorage.setItem('mode', JSON.stringify(mode))
 }
 
 if (modeStatus) {
-	check.click()
-	changeMode()
+  check.click()
+  changeMode()
 }
 
 check.onclick = changeMode
 
 // show menu
 menu.onclick = () => {
-	mobile.setAttribute('class', 'list__mobile')
-	fade.classList.add('back__drop')
+  mobile.setAttribute('class', 'list__mobile')
+  fade.classList.add('back__drop')
 }
 
 // hide menu
 fade.onclick = () => {
-	mobile.setAttribute('class', 'header__navigation')
-	fade.classList.remove('back__drop')
+  mobile.setAttribute('class', 'header__navigation')
+  fade.classList.remove('back__drop')
 }
